203eef286ef57141c7192b3828dc2a88558d0c2c
Bojan Bozilov <bobobozilov@gmail.com>
2023-08-29 10:59:58 +0200

    initial backend

---
+++ backend/shared-kernel/src/main/java/mk/ukim/finki/sharedkernel/SharedKernelApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.sharedkernel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedKernelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharedKernelApplication.class, args);
+    }
+
+}
