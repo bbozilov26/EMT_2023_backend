cb583f72e7acfdcd1ece3546aa200ff564bc8d45
Bojan Bozilov <bobobozilov@gmail.com>
2023-09-26 17:17:46 +0200

    hotfix

--- backend/user-management/src/main/java/mk/ukim/finki/usersmanagement/domain/models/ids/UserId.java
+++ backend/user-management/src/main/java/mk/ukim/finki/usersmanagement/domain/models/ids/UserId.java
@@ -9,11 +9,11 @@ public class UserId extends DomainObjectId {
         super(UserId.randomId(UserId.class).getId());
     }
 
-    public UserId(@NonNull Long uuid) {
+    public UserId(@NonNull String uuid) {
         super(uuid);
     }
 
-    public static UserId of(Long uuid) {
+    public static UserId of(String uuid) {
         return new UserId(uuid);
     }
 }
